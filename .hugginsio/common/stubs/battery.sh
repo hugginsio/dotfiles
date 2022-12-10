c8e4296fda599f37670673d3ff3142fe65a09fad
hugginsio <kylehuggins2.0@gmail.com>
2022-12-09 19:58:29 -0600

    Remove battery stub

--- .hugginsio/common/stubs/battery.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env bash
-
-if [[ $(uname -s) == "Darwin" ]]; then
-  pmset -g batt | grep -Eo "\d+%"
-else
-  echo "$(upower -i $(upower -e | grep BAT) | grep "percentage:" | grep -o '[[:digit:]]*%$')"
-fi
