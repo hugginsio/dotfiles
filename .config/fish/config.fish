bf56b5736c4a24c379fafce8cd4247f423cc519f
hugginsio <kylehuggins2.0@gmail.com>
2022-12-12 18:17:42 -0600

    WIP: move to fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,43 @@
+#!/usr/bin/env fish
+
+# fish
+
+set -Ux DOTFILES $HOME/.hugginsio-fish
+
+if test "$DOTFILES_ARE_CONFIGURED" != "true"
+  function dotfiles_source -d "Sources a configuration file if it exists"
+    for arg in $argv
+      if test -r $arg && test -f $arg
+        source $arg
+      end
+    end
+  end
+
+  ## load common configuration files
+
+  for FILE in $DOTFILES/common/*.fish
+    dotfiles_source $FILE
+  end
+
+  ## load host-specific configuration
+
+  dotfiles_source $DOTFILES/host/(hostname -s | string sub -l 4 | string lower).fish
+
+  ## load os-specific configuration
+
+  dotfiles_source $DOTFILES/os/(uname -s | string lower).fish
+
+  ## load application configuration files
+
+  for FILE in $DOTFILES/apps/*.fish
+    dotfiles_source $FILE
+  end
+
+  ## load extra configuration
+
+  dotfiles_source $DOTFILES/extra.fish
+
+  ## set DOTFILES_ARE_CONFIGURED
+  
+  set -Ux DOTFILES_ARE_CONFIGURED true
+end
---
+++ .hugginsio-fish/common/abbr.fish
@@ -0,0 +1,34 @@
+#!/usr/bin/env fish
+
+# common abbreviations
+
+abbr -a ... 'cd ../../'
+abbr -a .. 'cd ../'
+abbr -a ~ 'cd ~/'
+abbr -a c 'clear'
+abbr -a cdg 'cd "$HOME/Documents/Develop/"'
+abbr -a cdp 'cd "$HOME/Documents/Photograph/"'
+abbr -a l 'la'
+abbr -a la 'ls -loFcgha'
+abbr -a ld 'ls -loFcghd .*'
+abbr -a lssh '"$HOME/.ssh/known_hosts" | grep -o "^\S*" | sort -t: -u -k1,1'
+abbr -a myip 'curl icanhazip.com'
+abbr -a nuke 'rm -rf'
+abbr -a p 'ps -f'
+abbr -a please 'sudo'
+
+# app-specific abbreviations
+
+## tmux
+
+abbr -a ta 'tmux attach'
+abbr -a tl 'tmux ls'
+abbr -a tk 'tmux kill-server'
+abbr -a t 'tmux new'
+
+## neovim
+
+if command -qs nvim
+  abbr -a vi 'nvim'
+  abbr -a vim 'nvim'
+end
