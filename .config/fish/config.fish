025f7098df856fada740f48dbaf847701a8cb8d6
hugginsio <kylehuggins2.0@gmail.com>
2022-12-12 18:27:18 -0600

    Adjust fishconfig

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,45 +4,46 @@
 
 set -Ux DOTFILES $HOME/.hugginsio-fish
 
+function dotfiles_source -d "Sources a configuration file if it exists"
+  for arg in $argv
+    if test -r $arg && test -f $arg
+      source $arg
+    end
+  end
+end
+
 if test "$DOTFILES_ARE_CONFIGURED" != "true"
   ## global prompt
 
-  function dotfiles_source -d "Sources a configuration file if it exists"
-    for arg in $argv
-      if test -r $arg && test -f $arg
-        source $arg
-      end
-    end
-  end
-
-  ## load common configuration files
+  ### load common configuration files
 
   for FILE in $DOTFILES/common/*.fish
     dotfiles_source $FILE
   end
 
-  ## load host-specific configuration
+  ### load host-specific configuration
 
   dotfiles_source $DOTFILES/host/(hostname -s | string sub -l 4 | string lower).fish
 
-  ## load os-specific configuration
+  ### load os-specific configuration
 
   dotfiles_source $DOTFILES/os/(uname -s | string lower).fish
 
-  ## load application configuration files
+  ### load application configuration files
 
   for FILE in $DOTFILES/apps/*.fish
     dotfiles_source $FILE
   end
 
-  ## load extra configuration
+  ### load extra configuration
 
   dotfiles_source $DOTFILES/extra.fish
 
-  ## set DOTFILES_ARE_CONFIGURED
+  ### set DOTFILES_ARE_CONFIGURED
   
   set -Ux DOTFILES_ARE_CONFIGURED true
 else
   ## per-prompt configuration
+
   dotfiles_source $DOTFILES/common/prompt.fish
 end
