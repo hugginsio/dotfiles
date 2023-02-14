277a94767f477aaba114e44814d3a384b7a689d2
Kyle Huggins <kylehuggins2.0@gmail.com>
2023-02-13 20:08:41 -0600

    feat(nvim): remove deprecated lsp

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -5,19 +5,8 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  "sumneko_lua",
+  "lua_ls",
   "gopls",
-})
-
--- Fix Undefined global 'vim'
-lsp.configure("sumneko_lua", {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" }
-      }
-    }
-  }
 })
 
 lsp.nvim_workspace()
