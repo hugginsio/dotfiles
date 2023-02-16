7b5216b29234d84d11842b89bb376a84b34ab6e4
Kyle Huggins <kylehuggins2.0@gmail.com>
2023-02-16 12:26:41 -0600

    feat(nvim): remove lsp-zero

--- .config/nvim/lua/hugginsio/plugins.lua
+++ .config/nvim/lua/hugginsio/plugins.lua
@@ -23,31 +23,10 @@ return {
     end,
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "org" }
+        ensure_installed = { "json", "lua", "org", "markdown"}
       })
     end,
     event = "BufReadPost",
-  },
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    dependencies = {
-      -- LSP Support
-      "neovim/nvim-lspconfig",
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-
-      -- Autocompletion
-      "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lua",
-
-      -- Snippets
-      "L3MON4D3/LuaSnip",
-      "rafamadriz/friendly-snippets",
-    },
   },
   {
     "kylechui/nvim-surround",
