f7bd26965bb06389c55b0333c076e8e024535a74
Kyle Huggins <kylehuggins2.0@gmail.com>
2023-04-06 22:30:17 -0500

    feat(fish): remove DOTFILES_ARE_CONFIGURED

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -18,33 +18,22 @@ function fish_greeting
   clear
 end
 
-if not test "$DOTFILES_ARE_CONFIGURED" = true
-  ## global prompt
-  ### load common configuration files
-  dotfiles_source $DOTFILES/common/*.fish
+## global prompt
+### load common configuration files
+dotfiles_source $DOTFILES/common/*.fish
 
-  ### load host-specific configuration
-  dotfiles_source $DOTFILES/host/(hostname -s | string sub -l 4 | string lower).fish
+### load host-specific configuration
+dotfiles_source $DOTFILES/host/(hostname -s | string sub -l 4 | string lower).fish
 
-  ### load os-specific configuration
-  dotfiles_source $DOTFILES/os/(uname -s | string lower).fish
+### load os-specific configuration
+dotfiles_source $DOTFILES/os/(uname -s | string lower).fish
 
-  ### load application configuration files
-  dotfiles_source $DOTFILES/apps/*.fish
+### load application configuration files
+dotfiles_source $DOTFILES/apps/*.fish
 
-  ### load extra configuration
-  dotfiles_source $DOTFILES/extra.fish
+### load extra configuration
+dotfiles_source $DOTFILES/extra.fish
 
-  ### set DOTFILES_ARE_CONFIGURED
-  set -gx DOTFILES_ARE_CONFIGURED true
-else
-  ## per-prompt configuration
-  ### starship and the dotfiles util need to always be available
-  dotfiles_source $DOTFILES/common/prompt.fish $DOTFILES/common/dotfiles.fish
-end
-
-function on_exit --on-event fish_exit
-  if test (pgrep fish -c) = 1
-    set -e DOTFILES_ARE_CONFIGURED
-  end
-end
+## per-prompt configuration
+### starship and the dotfiles util need to always be available
+dotfiles_source $DOTFILES/common/prompt.fish $DOTFILES/common/dotfiles.fish
