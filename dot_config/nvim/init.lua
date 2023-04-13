07e5dccffa96282e9c17b341abea8d2d17509127
Kyle Huggins <kylehuggins2.0@gmail.com>
2023-04-12 20:46:43 -0500

    feat(nvim): move init to lua, install lazy

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,21 @@
+-- Source existing vim configuration (https://neovim.io/doc/user/nvim.html#nvim-from-vim)
+vim.cmd([[
+    set runtimepath^=~/.vim runtimepath+=~/.vim/after
+    let &packpath = &runtimepath
+    source ~/.vim/vimrc
+]])
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup(plugins, opts)
--- dot_config/nvim/init.vim
+++
@@ -1,4 +0,0 @@
-" Source existing vim configuration (https://neovim.io/doc/user/nvim.html#nvim-from-vim)
-set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath = &runtimepath
-source ~/.vim/vimrc
