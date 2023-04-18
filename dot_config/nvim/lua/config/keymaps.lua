24f73b7459b6063e82d9dac218ab939596c3dcc4
Kyle Huggins <kylehuggins2.0@gmail.com>
2023-04-18 08:24:35 -0500

    feat(nvim): enable g/zip plugins, move Lazy keymap

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,18 @@
 -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+
+local Util = require("lazyvim.util")
+
+local function map(mode, lhs, rhs, opts)
+    local keys = require("lazy.core.handler").handlers.keys
+    ---@cast keys LazyKeysHandler
+    -- do not create the keymap if a lazy keys handler exists
+    if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+        opts = opts or {}
+        opts.silent = opts.silent ~= false
+        vim.keymap.set(mode, lhs, rhs, opts)
+    end
+end
+
+-- lazy
+map("n", "<leader>l", "")
+map("n", "<leader>ol", "<cmd>:Lazy<cr>", { desc = "Lazy" })
--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -30,14 +30,12 @@ require("lazy").setup({
     performance = {
         rtp = {
             disabled_plugins = {
-                "gzip",
                 "matchit",
                 "matchparen",
                 "netrwPlugin",
                 "tarPlugin",
                 "tohtml",
                 "tutor",
-                "zipPlugin",
             },
         },
     },
