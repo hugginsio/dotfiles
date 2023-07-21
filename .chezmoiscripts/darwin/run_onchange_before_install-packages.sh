42c0a73da7cfa6316334eeb035e85cb7518cbea3
Kyle Huggins <kylehuggins2.0@gmail.com>
2023-07-20 22:02:46 -0500

    feat(macOS): automatically install brew and bundle

--- .chezmoiscripts/darwin/run_onchange_before_install-packages.sh
+++ .chezmoiscripts/darwin/run_onchange_before_install-packages.sh
@@ -5,3 +5,18 @@
 set -eu
 
 echo "Detected: macOS"
+echo "Installing essentials..."
+
+if [[ $(command -v brew) == "" ]]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    brew update && brew upgrade && brew cleanup
+fi
+
+brew bundle --no-lock --file=/dev/stdin <<EOF
+brew "coreutils"
+brew "curl"
+brew "git"
+brew "gpg"
+brew "gawk"
+EOF
