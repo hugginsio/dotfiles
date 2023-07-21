4f0da8d694c0237a54e4d5929caa2004590df129
Kyle Huggins <kylehuggins2.0@gmail.com>
2023-07-21 09:02:11 -0500

    feat(macOS): update brew installs

--- .chezmoiscripts/darwin/run_onchange_before_install-packages.sh
+++ .chezmoiscripts/darwin/run_onchange_before_install-packages.sh
@@ -7,16 +7,25 @@ set -eu
 echo "Detected: macOS"
 echo "Installing essentials..."
 
+mkdir -p ~/.local/bin
+
 if [[ $(command -v brew) == "" ]]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-    brew update && brew upgrade && brew cleanup
 fi
 
+brew update && brew upgrade && brew cleanup
 brew bundle --no-lock --file=/dev/stdin <<EOF
+brew "bat"
 brew "coreutils"
 brew "curl"
+brew "fish"
+brew "gawk"
+brew "gh"
 brew "git"
 brew "gpg"
-brew "gawk"
+brew "neovim"
+brew "ripgrep"
+brew "starship"
+cask "appcleaner"
+cask "raycast"
 EOF
