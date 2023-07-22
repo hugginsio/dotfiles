5981ba30bfdc318e9339166c4057d2797ce74f0e
Kyle Huggins <kylehuggins2.0@gmail.com>
2023-07-21 23:18:57 -0500

    feat(fish): add abbr, functions

---
+++ dot_config/hugginsio/fish/common/functions.fish
@@ -0,0 +1,14 @@
+#!/usr/bin/env fish
+
+function mcd -d "Creates a directory (if it does not exist) and set CWD"
+    command mkdir $argv
+    if test $status = 0
+        switch $argv[(count $argv)]
+            case '-*'
+
+            case '*'
+                cd $argv[(count $argv)]
+                return
+        end
+    end
+end
