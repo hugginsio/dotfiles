c498a37ad2aac2e8abd1f33311aaf961f639e871
Kyle Huggins <kylehuggins2.0@gmail.com>
2023-07-21 22:03:54 -0500

    feat: configure asdf-vm

---
+++ .chezmoiscripts/darwin/run_onchange_after_install-asdf.sh
@@ -0,0 +1,16 @@
+#!/bin/sh # vi: ft=bash
+
+# -e: exit on error
+# -u: exit on unset variables
+set -eu
+
+if [[ $(command -v asdf) == "" ]]; then
+    echo "ERR! asdf-vm is not installed. Try re-applying your dotfiles."
+    exit
+fi
+
+echo "Configuring asdf-vm $(asdf --version)..."
+
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git || true
+asdf install nodejs latest
+asdf global nodejs latest
