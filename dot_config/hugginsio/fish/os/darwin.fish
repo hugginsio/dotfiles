7fc35b5f35daa7e6a01a5b9da56eb0daed2690bb
Kyle Huggins <kylehuggins2.0@gmail.com>
2023-07-23 16:34:10 -0500

    feat(fish): add GNUBINS to PATH on darwin

--- dot_config/hugginsio/fish/os/darwin.fish
+++ dot_config/hugginsio/fish/os/darwin.fish
@@ -5,3 +5,8 @@
 fish_add_path /opt/homebrew/bin
 
 set -gx XDG_CONFIG_HOME "$HOME/.config"
+
+set GNUBINS (find (brew --prefix)/opt -type d -follow -name gnubin -print)
+for ITEM in $GNUBINS
+    fish_add_path $ITEM
+end
