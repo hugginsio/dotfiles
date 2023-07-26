963c13c56711794bd95f2a249c5a2d702367e77b
Kyle Huggins <kyle@huggins.io>
2023-07-25 23:15:26 -0500

    feat(fish) add gitignore.io function

--- dot_config/hugginsio/fish/common/functions.fish
+++ dot_config/hugginsio/fish/common/functions.fish
@@ -12,3 +12,7 @@ function mcd -d "Creates a directory (if it does not exist) and set CWD"
         end
     end
 end
+
+function gi -d "Interact with gitignore.io"
+    curl -sL https://www.toptal.com/developers/gitignore/api/$argv
+end
