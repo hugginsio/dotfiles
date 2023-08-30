3ed264881b66aa04fe16a429ce43dbf3c9d0937e
Kyle Huggins <kyle@huggins.io>
2023-08-29 20:13:42 -0500

    feat(fish): replace fisher with diy
    Slightly slower than fisher, but managing the plugins with Chezmoi is much easier.

--- .chezmoiscripts/common/run_once_after_install-fisher.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-
-if command -qs fisher
-    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update
-end
-
-# vi: ft=fish
--- .chezmoiscripts/common/run_onchange_fisher-update.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env fish
-
-# hash: {{ include "dot_config/fish/fish_plugins" | sha256sum }}
-
-# HACK: remove all plugins except fisher to prevent update failures.
-# This can be removed once a --force flag is added.
-fisher list [^jorgebucaran/fisher] | fisher remove
-fisher update
