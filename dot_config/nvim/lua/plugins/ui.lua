06524c40829d8206fc4fbd5e9429c89ab5032e0b
Kyle Huggins <kyle@huggins.io>
2023-10-12 21:11:37 -0500

    fix(lazyvim): move neo-tree to editor, migrate opt

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -9,36 +9,6 @@ return {
         priority = 100,
     },
     {
-        "nvim-neo-tree/neo-tree.nvim",
-        opts = {
-            filesystem = {
-                bind_to_cwd = false,
-                follow_current_file = true,
-            },
-            window = {
-                width = 30,
-                mappings = {
-                    ["<space>"] = "none",
-                },
-            },
-            popup_border_style = "single",
-            default_component_configs = {
-                indent = {
-                    with_expanders = true,
-                },
-                git_status = {
-                    symbols = {
-                        modified = "!",
-                        deleted = "✖",
-                        renamed = "»",
-                        untracked = "?",
-                        conflict = "=",
-                    },
-                },
-            },
-        },
-    },
-    {
         "nvim-lualine/lualine.nvim",
         opts = function()
             local icons = require("lazyvim.config").icons
