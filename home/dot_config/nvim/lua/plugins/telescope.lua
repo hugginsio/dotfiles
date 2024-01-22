d1ad5ca48d9ff621db3d30c21b3ab06a4d99799d
Kyle Huggins <kyle@huggins.io>
2024-01-21 19:13:39 -0600

    feat(nvim): fix telescope fzf native

--- home/dot_config/nvim/lua/plugins/telescope.lua
+++ home/dot_config/nvim/lua/plugins/telescope.lua
@@ -9,6 +9,11 @@ return {
             {
                 "nvim-telescope/telescope-fzf-native.nvim",
                 build = "make",
+                config = function()
+                    Util.on_load("telescope.nvim", function()
+                        require("telescope").load_extension("fzf")
+                    end)
+                end,
                 enabled = vim.fn.executable("make") == 1,
             },
         },
--- home/dot_config/nvim/lua/util/init.lua
+++ home/dot_config/nvim/lua/util/init.lua
@@ -12,4 +12,22 @@ function M.error(msg)
     vim.notify(msg, vim.log.levels.ERROR)
 end
 
+-- https://github.com/LazyVim/LazyVim/blob/eccf3b5e68035516c879b3e2b4d6bf8512c87976/lua/lazyvim/util/init.lua#L129
+function M.on_load(name, fn)
+    local Config = require("lazy.core.config")
+    if Config.plugins[name] and Config.plugins[name]._.loaded then
+        fn(name)
+    else
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "LazyLoad",
+            callback = function(event)
+                if event.data == name then
+                    fn(name)
+                    return true
+                end
+            end,
+        })
+    end
+end
+
 return M
