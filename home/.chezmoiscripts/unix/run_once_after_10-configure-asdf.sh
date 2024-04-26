75878685718ea3c5e4547ebeced10b21283d107d
Kyle Huggins <kyle.huggins@proton.me>
2024-04-25 21:34:06 -0500

    Revert "fix(asdf): swap script conditions"
    This reverts commit 4557fc0c8f18101e2f57c728026b5f9e1865f35e.

--- home/.chezmoiscripts/unix/run_once_after_10-configure-asdf.sh
+++ home/.chezmoiscripts/unix/run_once_after_10-configure-asdf.sh
@@ -7,11 +7,11 @@ set -eu
 
 if ! command -v asdf &> /dev/null
 then
+    echo "ERR! asdf-vm is not installed. Try re-applying your dotfiles."
+else
     echo "Configuring asdf-vm $(asdf --version)..."
     asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git || true
 
     asdf plugin add python https://github.com/asdf-community/asdf-python.git || true
     asdf install python latest
-else
-    echo "ERR! asdf-vm is not installed. Try re-applying your dotfiles."
 fi
