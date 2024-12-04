0b0b4b2e3f7c10d65ce80623a5ced315a3605c42
Kyle Huggins <kyle@huggins.io>
2024-12-03 18:53:16 -0600

    feat: update paths

--- home/dot_config/fish/autosource/paths.fish
+++ home/dot_config/fish/autosource/paths.fish
@@ -1,10 +1,11 @@
 #!/usr/bin/env fish
 
 set -l PATHS_LIST "$HOME/bin/" \
-    "$HOME/.local/bin/" \
     "$HOME/.nix-profile/bin" \
     "/nix/var/nix/profiles/default/bin" \
+    "$HOME/.local/bin/" \
     "$HOME/go/bin" \
+    "$HOME/.krew/bin" \
 
 for P in $PATHS_LIST
     fish_add_path $P
