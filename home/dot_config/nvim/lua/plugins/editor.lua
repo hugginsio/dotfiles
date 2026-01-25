012f9b2c6ce902968957b4d466661d0df2f4f8d9
Kyle Huggins <kyle@huggins.io>
2026-01-25 14:11:25 -0600

    feat: add tpope/vim-eunuch

---
+++ home/dot_config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,19 @@
+---@type LazySpec
+return {
+  {
+    "tpope/vim-eunuch",
+    cmd = {
+      "Cfind",
+      "Chmod",
+      "Clocate",
+      "Delete",
+      "Lfind",
+      "Mkdir",
+      "Move",
+      "Remove",
+      "SudoEdit",
+      "SudoWrite",
+      "Wall",
+    },
+  },
+}
