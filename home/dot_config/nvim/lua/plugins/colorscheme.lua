0a7e6bd787eed6632b32e9db957dabf9038fefef
Kyle Huggins <kyle@huggins.io>
2026-02-19 20:53:16 -0600

    feat: dynamic colorscheme

--- home/dot_config/nvim/lua/plugins/colorscheme.lua
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -4,8 +4,21 @@ return {
     "catppuccin/nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme catppuccin-mocha]])
-    end,
   },
+  {
+    "f-person/auto-dark-mode.nvim",
+    lazy = false,
+    priority = 999,
+    opts = {
+      set_dark_mode = function()
+        vim.api.nvim_set_option_value("background", "dark", {})
+        vim.cmd("colorscheme catppuccin-mocha")
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option_value("background", "light", {})
+        vim.cmd("colorscheme catppuccin-latte")
+      end,
+      fallback = "dark"
+    }
+  }
 }
